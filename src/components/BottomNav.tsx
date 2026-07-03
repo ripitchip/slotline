1fb040c672c51ccfb3f113a03a0e64f96eaad8a0
ripitchip <thomasderudder@sfr.fr>
2026-07-03 16:31:57 +0200

    style(mobile): simplify bottom navigation by keeping only list (grid) and plus (create)

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -1,9 +1,6 @@
 import {
   PlusCircle,
-  Lock,
   Grid,
-  Github,
-  QuestionCircle,
 } from "react-bootstrap-icons";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -22,15 +19,6 @@ const BottomNav = (): JSX.Element => {
           <Grid className="bottom-nav-link-icon" />
         </a>
       </Link>
-      <Link href="/how-to">
-        <a
-          className={`bottom-nav-link ${
-            router.pathname === "/how-to" ? ` active` : ``
-          }`}
-        >
-          <QuestionCircle className="bottom-nav-link-icon" />
-        </a>
-      </Link>
       <Link href="/">
         <a
           className={`bottom-nav-link ${
@@ -38,20 +26,6 @@ const BottomNav = (): JSX.Element => {
           }`}
         >
           <PlusCircle className="bottom-nav-link-icon" />
-        </a>
-      </Link>
-      <Link href="https://github.com/samayapp/samay">
-        <a className="bottom-nav-link">
-          <Github className="bottom-nav-link-icon" />
-        </a>
-      </Link>
-      <Link href="/privacy">
-        <a
-          className={`bottom-nav-link ${
-            router.pathname === "/privacy" ? ` active` : ``
-          }`}
-        >
-          <Lock className="bottom-nav-link-icon" />
         </a>
       </Link>
     </div>
